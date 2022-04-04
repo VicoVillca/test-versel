df34e8a03cfb92a53fdd5f2ae9064e02eddc933a
VicoVillca <vicovillca@hotmail.com>
2022-04-04 18:56:01 -0400

    Start

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Saludando desde vercel.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Holap 
         </a>
       </header>
     </div>
